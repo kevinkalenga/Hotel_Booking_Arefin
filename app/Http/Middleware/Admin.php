1ee2213a18cb65ef7e666b49b1bf09925ff76975
kevin <kevinkalenga10@gmail.com>
2026-03-07 15:35:55 +0100

    Admin and Auth middleware

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Vérifie si l'admin est connecté avec le bon guard
+        if (!Auth::check()) {
+            return redirect()->route('admin_login');
+        }
+        return $next($request);
+    }
+}
