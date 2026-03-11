1aaaf41ce1cfa39fc5004e67e92712b9f5143d31
kevin <kevinkalenga10@gmail.com>
2026-03-11 13:18:26 +0100

    Testimonial - Part2

--- app/Http/Controllers/Admin/AdminTestimonialController.php
+++ app/Http/Controllers/Admin/AdminTestimonialController.php
@@ -13,4 +13,36 @@ class AdminTestimonialController extends Controller
         $testimonials = Testimonial::get();
         return view('admin.testimonial_view', compact('testimonials'));
     }
+
+    public function add()
+    {
+        return view('admin.testimonial_add');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'photo' => ['required', 'image', 'mimes:jpg,jpeg,png,gif,webp', 'max:2048'],
+            'name' => 'required',
+            'designation' => 'required',
+            'comment' => 'required',
+        ]);
+
+        $final_name = null;
+
+        if ($request->hasFile('photo')) {
+            $ext = $request->file('photo')->extension();
+            $final_name = time() . '.' . $ext;
+            $request->file('photo')->move(public_path('uploads/'), $final_name);
+        }
+
+        $obj = new Testimonial();
+        $obj->photo = $final_name;
+        $obj->name = $request->name;
+        $obj->designation = $request->designation;
+        $obj->comment = $request->comment;
+        $obj->save();
+
+        return redirect()->back()->with('success', 'Testimonial added successfully');
+    }
 }
