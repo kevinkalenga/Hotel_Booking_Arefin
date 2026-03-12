f6cd4a8b4428d30477b5cb93f550d3a1d81859a0
kevin <kevinkalenga10@gmail.com>
2026-03-13 00:38:12 +0100

    Photo Gallery - Part4

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use App\Http\Controllers\Admin\AdminPhotoController;
 use App\Http\Controllers\Front\HomeController;
 use App\Http\Controllers\Front\AboutController;
 use App\Http\Controllers\Front\BlogController;
+use App\Http\Controllers\Front\PhotoController;
 
 /* ------------------------- Front--------------------- */
 
@@ -21,6 +22,8 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/about', [AboutController::class, 'index'])->name('about');
 Route::get('/blog', [BlogController::class, 'index'])->name('blog');
 Route::get('/post/{id}', [BlogController::class, 'single_post'])->name('post');
+Route::get('/photo-gallery', [PhotoController::class, 'index'])->name('photo_gallery');
+
 
 
 
