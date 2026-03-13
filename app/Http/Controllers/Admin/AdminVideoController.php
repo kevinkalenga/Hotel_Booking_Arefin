17508d8baf36e7e7508c804cad806a6ca9539044
kevin <kevinkalenga10@gmail.com>
2026-03-13 01:02:02 +0100

    Video Gallery - Part2

--- app/Http/Controllers/Admin/AdminVideoController.php
+++ app/Http/Controllers/Admin/AdminVideoController.php
@@ -13,4 +13,25 @@ class AdminVideoController extends Controller
         $videos = Video::get();
         return view('admin.video_view', compact('videos'));
     }
+
+    public function add()
+    {
+        return view('admin.video_add');
+    }
+
+
+    public function store(Request $request)
+    {
+            $request->validate([
+            'video_id' => 'required',
+             
+        ]);
+
+        $obj = new Video();
+        $obj->video_id = $request->video_id;
+        $obj->caption = $request->caption;
+        $obj->save();
+
+        return redirect()->back()->with('success', 'Video is added Successfully');
+    }
 }
