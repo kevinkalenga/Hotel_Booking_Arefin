e43eae38143a66d6de2fbe1b84879276e65a532f
kevin <kevinkalenga10@gmail.com>
2026-03-14 02:06:07 +0100

    FAQ - Part3

--- app/Http/Controllers/Admin/AdminFaqController.php
+++ app/Http/Controllers/Admin/AdminFaqController.php
@@ -33,4 +33,32 @@ class AdminFaqController extends Controller
 
         return redirect()->back()->with('success', 'FAQ is added Successfully');
     }
+
+    public function edit($id)
+    {
+        // check all the items from the slide
+        $faq_data = Faq::where('id', $id)->first();
+
+        return view('admin.faq_edit', compact('faq_data'));
+    }
+
+
+    public function update(Request $request, $id)
+    {
+       $obj = Faq::where('id', $id)->first();
+
+        $obj->question = $request->question;
+        $obj->answer = $request->answer;
+        $obj->save();
+
+        return redirect()->back()->with('success', 'FAQ updated successfully');
+    }
+
+    public function delete($id)
+    {
+        $single_data = Faq::where('id', $id)->first();
+        $single_data->delete();
+
+         return redirect()->back()->with('success', 'Faq deleted successfully');
+    }
 }
