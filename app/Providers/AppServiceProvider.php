c15a456c4c9b36c69c8bc6b279538016f384fde3
kevin <kevinkalenga10@gmail.com>
2026-03-15 01:47:09 +0100

    Page - About - Part2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use App\Models\Page;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -20,5 +21,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
          Paginator::useBootstrap();
+
+           // Optional: load shared data(get all information)
+        $page_data = Page::where('id', 1)->first();
+
+         // Share data globally with all views
+        view()->share('global_page_data', $page_data);
     }
 }
--- resources/views/front/layout/app.blade.php
+++ resources/views/front/layout/app.blade.php
@@ -72,9 +72,13 @@
                                 <li class="nav-item">
                                     <a href="{{route('home')}}" class="nav-link">Home</a>
                                 </li>
-                                <li class="nav-item">
-                                    <a href="{{route('about')}}" class="nav-link">About</a>
-                                </li>
+                                 @if($global_page_data->about_status == 1)
+                                  <li class="nav-item">
+                                    <a href="{{route('about')}}" class="nav-link">
+                                        {{$global_page_data->about_heading}}
+                                    </a>
+                                  </li>
+                                @endif
                                 <li class="nav-item">
                                     <a href="javascript:void;" class="nav-link dropdown-toggle">Room & Suite</a>
                                     <ul class="dropdown-menu">
